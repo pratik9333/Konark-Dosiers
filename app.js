5f811793c5f7b950340a3ca85db96d77938f896b
Pratik <pratikaswani9333@gmail.com>
2021-10-13 17:11:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+// DB Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+const port = process.env.PORT || 8000; // PORT
+
+app.listen(port, () => {
+  console.log(`app is runnning at ${port}`);
+});
